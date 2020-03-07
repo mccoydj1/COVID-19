f5bc8e687fe280a7f0342c35c8a47d820899da4b
Darin <mccoydj1@gmail.com>
2020-03-07 00:45:41 -0600

    added more detail / temperature information

--- cv_stats.py
+++ cv_stats.py
@@ -1,28 +1,40 @@
 import csv
 import numpy as np
 from operator import itemgetter
+import requests, json
+import pytemperature
 
-def cv_stats(cvpath):
+
+def cv_stats(cvpath, apikey):
 
     with open(cvpath, newline='\n') as f:
         reader = csv.reader(f)
         data = list(reader)
 
+    header = data[0]
+    data = [fixdataquality(row) for row in data[1:]]
+
     countries = [row[1] for row in data]
-    countriesregion = [row[0:2] for row in data[1:-1]]
+    countriesregion = [row[0:4] for row in data[1:-1]]
 
     for i in countriesregion:
         if i[0] == "":
             i[0] = i[1]
+        elif i[0].find(',')<0:
+            i[0] = i[0] + ', ' + i[1]
 
     uniq_countries = list(set(countries[1:-1]))
     uniq_countries_num = len(uniq_countries)
 
+
     usdata = [[data[0], float(data[-1])] for data in data if data[1] == 'US']
+    ildata = [[data[0], float(data[-1])] for data in data if data[0].find('IL') > 0]
+    ilcases = sum([row[1] for row in ildata])
+
     usdata = sorted(usdata, key=itemgetter(1), reverse=True)
     uscases = sum([row[1] for row in usdata])
 
-    lastfourdays = [row[-5:-1] for row in data]
+    lastfourdays = [row[-4:] for row in data]
 
     finallist = list()
 
@@ -31,65 +43,182 @@ def cv_stats(cvpath):
 
         countrydata_num = [float(i) for i in countrydata]
         suspect_country = countriesregion[ct][0]
-        increase1, increase2, amt1, amt2, amt3, acc, increasetype = assess_increase(countrydata_num)
-        finallist.append([suspect_country, increase1, increase2, amt1, amt2, amt3, acc, increasetype])
+
+        increase1, increase2, increase3, amt1, amt2, amt3, amt4, acc, increasetype = assess_increase(countrydata_num)
+
+        if len(increasetype)>0:
+
+            lat = countriesregion[ct][2]
+            long = countriesregion[ct][3]
+            temp = assess_weather(lat, long, apikey)
+
+            finallist.append([suspect_country, increase1, increase2, increase3, amt1, amt2, amt3, amt4, acc, temp, increasetype])
 
         ct = ct + 1
 
-    emaillist = [row for row in finallist if row[1] > 0]
-
-    return emaillist, uniq_countries_num, usdata, uscases
+    return finallist, uniq_countries_num, usdata, uscases, ildata, ilcases
 
 
 def assess_increase(raw_countrydata_num):
 
-    countrydata_num = smooth(raw_countrydata_num, 2)
+    #countrydata_num = smooth(raw_countrydata_num, 2)
+    countrydata_num = raw_countrydata_num
 
     increase1 = 0
     increase2 = 0
+    increase3 = 0
     amt1 = 0
     amt2 = 0
     amt3 = 0
+    amt4 = 0
     acc = 0
     increasetype = ''
+
+    np.diff(raw_countrydata_num)
 
     # Assume the very first data point is greater than 0 and the last data point is greater than 10
     if countrydata_num[0] > 0 and countrydata_num[2] > 10:
 
-        delta1 = countrydata_num[1] - countrydata_num[0]
-        delta2 = countrydata_num[2] - countrydata_num[1]
+        delta1 = countrydata_num[-3] - countrydata_num[-4]
+        delta2 = countrydata_num[-2] - countrydata_num[-3]
+        delta3 = countrydata_num[-1] - countrydata_num[-2]
 
-        perc_increase1 = 100 * delta1 / countrydata_num[0]
-        perc_increase2 = 100 * delta2 / countrydata_num[1]
+        perc_increase1 = 100 * delta1 / countrydata_num[-4]
+        perc_increase2 = 100 * delta2 / countrydata_num[-3]
+        perc_increase3 = 100 * delta3 / countrydata_num[-2]
 
-        largecase = delta1 > 500 and delta1 > 500
-        normalincrease = delta1 > 10 and delta2 > 10 and perc_increase1 > 10 and perc_increase2 > 10
-        temp_acc = delta2 - delta1
+
+        # Call it a large case if 2/3 have increases greater than 500
+        largecase = sum([delta1 > 500, delta2 > 500, delta3 > 500]) >= 2
+        normalincrease_amt = sum([delta1 > 10, delta2 > 10, delta3 > 10]) >= 2
+        rapidincrease_pct = sum([perc_increase1 > 30, perc_increase2 > 30, perc_increase3 > 30]) >= 2
+        slowincrease_pct = sum([perc_increase1 > 5, perc_increase2 > 5, perc_increase3 > 5]) >= 2
+
+        temp_acc1 = delta2 - delta1
+        temp_acc2 = delta3 - delta2
+        temp_acc = max(temp_acc1, temp_acc2)
 
         # If you find a likely suspect
-        if largecase or normalincrease:
+        if largecase or (rapidincrease_pct and normalincrease_amt):
             increase1 = perc_increase1
             increase2 = perc_increase2
-            amt1 = raw_countrydata_num[-3]
-            amt2 = raw_countrydata_num[-2]
-            amt3 = raw_countrydata_num[-1]
-            acc = delta2 - delta1
+            increase3 = perc_increase3
+            amt1 = raw_countrydata_num[-4]
+            amt2 = raw_countrydata_num[-3]
+            amt3 = raw_countrydata_num[-2]
+            amt4 = raw_countrydata_num[-1]
+            acc = temp_acc
             increasetype = 'rapid'
 
-        elif perc_increase1 > 10 and perc_increase2 > 10 and temp_acc > 0:
+        elif largecase or (slowincrease_pct and normalincrease_amt):
             increase1 = perc_increase1
             increase2 = perc_increase2
-            amt1 = raw_countrydata_num[-3]
-            amt2 = raw_countrydata_num[-2]
-            amt3 = raw_countrydata_num[-1]
-            acc = delta2 - delta1
+            increase3 = perc_increase3
+            amt1 = raw_countrydata_num[-4]
+            amt2 = raw_countrydata_num[-3]
+            amt3 = raw_countrydata_num[-2]
+            amt4 = raw_countrydata_num[-1]
+            acc = temp_acc
+            increasetype = 'slow'
+
+        elif rapidincrease_pct and temp_acc > 0:
+            increase1 = perc_increase1
+            increase2 = perc_increase2
+            increase3 = perc_increase3
+            amt1 = raw_countrydata_num[-4]
+            amt2 = raw_countrydata_num[-3]
+            amt3 = raw_countrydata_num[-2]
+            amt4 = raw_countrydata_num[-1]
+            acc = temp_acc
             increasetype = 'emerging'
 
+        elif countrydata_num[-4] > 100:
+            increase1 = perc_increase1
+            increase2 = perc_increase2
+            increase3 = perc_increase3
+            amt1 = raw_countrydata_num[-4]
+            amt2 = raw_countrydata_num[-3]
+            amt3 = raw_countrydata_num[-2]
+            amt4 = raw_countrydata_num[-1]
+            acc = temp_acc
+            increasetype = 'stable'
+
     # Return all values
-    return increase1, increase2, amt1, amt2, amt3, acc, increasetype
+    return increase1, increase2, increase3, amt1, amt2, amt3, amt4, acc, increasetype
 
 
 def smooth(y, box_pts):
     box = np.ones(box_pts)/box_pts
     y_smooth = np.convolve(y, box, mode='valid')
     return y_smooth
+
+# Fix the quality of the data
+def fixdataquality(row):
+    if row[-1] == '':
+        row[-1] = 0
+
+    return row
+
+
+def createtable(title, data):
+    text = "<b>" + title + ' (' + str(len(data)) + ')' +  "</b><table><tr><th>Country</th><th>%3DA</th><th>%2DA</th><th>%Y</th><th>4DA</th><th>3DA</th><th>2DA</th><th>Yest</th><th>Acc</th></tr>"
+
+    for country in data:
+        text = text + "<tr><td>%s (%.1fF)</td><td>%.1f</td><td>%.1f</td><td>%.1f</td>" \
+                      "<td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%.f</td></tr>" % (
+                   country[0], country[-2], country[1], country[2], country[3], country[4], country[5], country[6], country[7], country[8])
+
+    text = text + "</table><br><br>"
+
+    return text
+
+def assess_weather(lat, long, api_key):
+
+    # base_url variable to store url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    # complete_url variable to store
+    # complete url address
+    #complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    complete_url = base_url + "lat=" + lat + '&lon=' + long +  "&appid=" + api_key
+
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
+
+    # json method of response object
+    # convert json format data into
+    # python format data
+    x = response.json()
+
+    # Now x contains list of nested dictionaries
+    # Check the value of "cod" key is equal to
+    # "404", means city is found otherwise,
+    # city is not found
+    if x["cod"] != "404":
+        # store the value of "main"
+        # key in variable y
+        y = x["main"]
+
+        # store the value corresponding
+        # to the "temp" key of y
+        current_temperature = pytemperature.k2f(y["temp"])
+
+        # store the value corresponding
+        # to the "pressure" key of y
+        current_pressure = y["pressure"]
+
+        # store the value corresponding
+        # to the "humidity" key of y
+        current_humidiy = y["humidity"]
+
+        # store the value of "weather"
+        # key in variable z
+        z = x["weather"]
+
+        # store the value corresponding
+        # to the "description" key at
+        # the 0th index of z
+        weather_description = z[0]["description"]
+
+        return current_temperature
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from cv_stats import cv_stats
+from cv_stats import cv_stats, createtable
 from operator import itemgetter
 import yaml
 import git
 import os
+import datetime
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -18,13 +19,19 @@ g.pull()
 cvpath = git_dir + r'\csse_covid_19_data\csse_covid_19_time_series\time_series_19-covid-Confirmed.csv'
 
 #Grab the latest stats
-data, uniq_countries_num, usdata, uscases = cv_stats(cvpath)
+data, uniq_countries_num, usdata, uscases, ildata, ilcases = cv_stats(cvpath, cfg['apikey'])
 
-emergingdata = [row for row in data if row[7] == 'emerging']
-emergingdata = sorted(emergingdata, key=itemgetter(6), reverse=True)
+emergingdata = [row for row in data if row[-1] == 'emerging']
+emergingdata = sorted(emergingdata, key=itemgetter(-3), reverse=True)
 
-rapiddata = [row for row in data if row[7] == 'rapid']
-rapiddata = sorted(rapiddata, key=itemgetter(6), reverse=True)
+rapiddata = [row for row in data if row[-1] == 'rapid']
+rapiddata = sorted(rapiddata, key=itemgetter(-3), reverse=True)
+
+slowdata = [row for row in data if row[-1] == 'slow']
+slowdata = sorted(slowdata, key=itemgetter(-3), reverse=True)
+
+stabledata = [row for row in data if row[-1] == 'stable']
+stabledata = sorted(stabledata, key=itemgetter(-3), reverse=True)
 
 
 #Send the email
@@ -32,9 +39,18 @@ sender_email = cfg['email']['user']
 password = cfg['email']['pass']
 
 #Who is the email going to send to?
-receiver_email = [cfg['to']['to1'], cfg['to']['to2']]
-#receiver_email = cfg['to']['to1']
 
+debug = True
+if debug:
+    receiver_email = [cfg['to']['to1']]
+else:
+    # If today is a saturday
+    if datetime.datetime.today().weekday() == 5:
+        receiver_email = [cfg['to']['to1'], cfg['to']['to2'], cfg['to']['to3']]
+    else:
+        receiver_email = [cfg['to']['to1'], cfg['to']['to2']]
+
+# Send the email
 message = MIMEMultipart("alternative")
 message["Subject"] = "Daily Coronavirus"
 message["From"] = sender_email
@@ -45,34 +61,23 @@ message["To"] = ", ".join(receiver_email)
 html = "<html><body>Total countries with coronavirus: " + str(uniq_countries_num) + "<br>" + \
        "Total # of US cases: %d ( %d cities) <br><br>" % (uscases, len(usdata))
 
-rapidspread = "<b>Rapid Spread</b><table><tr><th>Country</th><th>%3DA</th><th>%2DA</th><th>3 days ago</th><th>2 days ago</th><th>Acc</th></tr>"
+rapidspread = createtable('Rapid Spread', rapiddata)
+emerging = createtable('Emerging', emergingdata)
+slow = createtable('Slow', slowdata)
+stable = createtable('Stable', stabledata)
 
-
-for country in rapiddata:
-    rapidspread = rapidspread + "<tr><td>%s</td><td>%.1f</td><td>%.1f</td><td>%d</td>" \
-              "<td>%d</td><td>%.f</td></tr>" % (country[0], country[1], country[2], country[4], country[5], country[6])
-
-rapidspread = rapidspread + "</table><br><br>"
-
-
-emerging = "<b>Emerging</b><table><tr><th>Country</th><th>%3DA</th><th>%2DA</th><th>3 days ago</th><th>2 days ago</th><th>Acc</th></tr>"
-
-for country in emergingdata:
-    emerging = emerging + "<tr><td>%s</td><td>%.1f</td><td>%.1f</td><td>%d</td>" \
-              "<td>%d</td><td>%.f</td></tr>" % (country[0], country[1], country[2], country[4], country[5], country[6])
-
-emerging = emerging + "</table><br><br>"
-
+ilcities = "<b>IL Cities</b><table><tr><th>City</th><th>Count</th></tr>"
+for city in ildata:
+    ilcities = ilcities + "<tr><td>%s</td><td>%d</td></tr>" %(city[0], city[1])
+ilcities = ilcities + "</table><br><br>"
 
 uscities = "<b>US Cities</b><table><tr><th>City</th><th>Count</th></tr>"
-
 for city in usdata:
     uscities = uscities + "<tr><td>%s</td><td>%d</td></tr>" %(city[0], city[1])
 uscities = uscities + "</table><br><br>"
 
 
-
-html = html + rapidspread + emerging + uscities
+html = html + ilcities + rapidspread + slow + emerging + stable + uscities
 
 # Add HTML/plain-text parts to MIMEMultipart message
 # The email client will try to render the last part first
